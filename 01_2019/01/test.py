a52e837a65f4bd8c243d42b3f17bd6f129e78222
Wonjin <terpe66@gmail.com>
2019-01-10 12:37:34 +0900

    ss3 | hangman -ing | 190110

--- 01_2019/01/test.py
+++ 01_2019/01/test.py
@@ -1,93 +1,43 @@
-def antant(n, H1=[], H2=[], H3=[], count=0):
+def hangman_list():
+    answer = input("맞혀야 할 단어를 입력하세요. : ")
+    blank = []
+    answer_list = []
+    for i in answer:
+        blank.append(i)
+        answer_list.append(i)
+    print(f"총 단어의 수는 {len(answer)}개 입니다. {('_ ' * len(answer)).strip()}")
+    count = 8
     
-    if n < 2:
-        return n
-# 1) 1번에 n 숫자 넣음    
-    if H1 == [] and H2 == [] and H3 == []:
-        for top in range(n):
-            H1 += [top]
+    while count != 0:
+        letter = input("이 단어가 맞나요? : ")
+        if letter in answer:
+            for i in answer:
+                if letter == i:
+                    blank(insert(answer_list.index(letter), letter)
+                    blank.pop(answer_list.index(letter))
+                    
+                else:
+                    while blank.count(i) != 0:
+                        blank.insert(blank.index(i), "._")
+                        blank.remove(i)
+                    
+                    if "._" in answer_list:
+                        for al in answer_list:
+                            if al != "._":
+                                blank.insert(answer_list.index(al), al)
+                                blank.pop(answer_list.index(al)+1)
+                
+
+            print(f"단어를 맞췄습니다. 현재 상태는 {''.join(blank)}, 남은 목숨은 {count}입니다.")
             
-    while True:
-        if len(H2) == n or len(H3) == n:
-            break
-    # 10) 1이 비어있을 때 2가 3보다 작으면 >>> 2를 3으로
-        if H1 == []:
-            if H2[0] < H3[0]:
-                H3.insert(0, H2[0])
-                del H2[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 11) 1이 비어있을 때 2가 3보다 크면 >>> 2를 1로
-            if H2[0] > H3[0]:
-                H1.insert(0, H2[0])
-                del H2[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 2) 2가 비어있을 때 >>> 1을 2로    
-        if H2 == []:
-            H2.insert(0, H1[0])
-            del H1[0]
-            count += 1
-            return antant(n, H1, H2, H3, count)
-    # 3) 3이 비어있고 1이 2보다 크면 >>> 1을 3으로    
-        if H3 == []:
-            if H1[0] > H2[0]:
-                H3.insert(0, H1[0])
-                del H1[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 4) 3이 비어있고 1이 2보다 작으면 >>> 1을 2로        
-            if H1[0] < H2[0]:
-                H2.insert(0, H1[0])
-                del H1[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 13 3이 비어있지 않고 1이 2보다 크고 3보다 작으면 >>> 1을 3으로
-        if H3 != []:
-            if H1[0] > H2[0] and H1[0] < H3[0]:
-                H3.insert(0, H1[0])
-                del H1[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 5) 3이 비어있지 않고 1이 2보다 크고 2가 3보다 작으면 >>> 2를 3으로
-            if H1[0] > H2[0] and H2[0] < H3[0]:
-                H3.insert(0, H2[0])
-                del H2[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 6) 3이 비어있지 않고 3이 1보다 작으면 >>> 3을 1로
-            if H3[0] < H1[0]:
-                H1.insert(0, H3[0])
-                del H3[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 7) 3이 비어있지 않고 3이 2보다 작으면 >>> 3을 2로        
-            if H3[0] < H2[0]:
-                H2.insert(0, H3[0])
-                del H3[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 12) 3이 비어있지 않고 2가 1보다 크면 >>> 2를 3으로
-            if H2[0] > H1[0]:
-                H3.insert(0, H2[0])
-                del H2[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
-    # 9) 3이 비어있지 않고 1이 2보다 작으면 >>> 1을 2로        
-            if H1[0] < H2[0]:
-                H2.insert(0, H1[0])
-                del H1[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
+        else:
+            count -= 1
+            print(f"틀렸습니다. 현재 상태는 {''.join(blank)}, 남은 목숨은 {count}입니다.")
+        
+        if "._" not in blank:
+            print("축하합니다! 살아남았습니다!")
 
-    # 8) 3이 비어있지 않고 2가 3보다 크고 1이 비어있으면 >>> 2를 1로
-            if H2[0] > H3[0] and H1 == []:
-                H1.insert(0, H2[0])
-                del H2[0]
-                count += 1
-                return antant(n, H1, H2, H3, count)
+    print(f"망해라!!!")
+        
 
-    return count
-    
-
-antant(5)
+hangman_list()
