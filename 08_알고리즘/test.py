6279277e7a49523f0f9565b314f08c5ca20cf1e3
Wonjin <terpe66@gmail.com>
2019-02-15 15:53:23 +0900

    ss3 | algorithm | 190215

--- 08_알고리즘/test.py
+++ 08_알고리즘/test.py
@@ -1,22 +1,14 @@
-test_num = 1
+import sys
+sys.stdin = open("input.txt")
 
-s_len = int(input())
-s_list = list(map(int, input().split()))
+for test_case in range(1, 11):
+    N = int(input())
+    arr = list(map(int, input().split()))
 
-sight_sum = 0
-for i in range(2, s_len - 2):
-    sight = 0
-    if s_list[i] - s_list[i-1] > 0 and s_list[i] - s_list[i-2] > 0:
-        left_sight = s_list[i] - max(s_list[i-1], s_list[i-2])
-        if s_list[i] - s_list[i+1] > 0 and s_list[i] - s_list[i+2] > 0:
-            right_sight = s_list[i] - max(s_list[i+1], s_list[i+2])
-            if left_sight > right_sight:
-                sight_sum += right_sight
-            else:
-                sight_sum += left_sight
-
-
-print(f"#{test_num} {sight_sum}")
-test_num += 1
-
+    ans = 0
+    for i in range(2, N - 2):
+        maxH = max(arr[i - 2], arr[i - 1], arr[i + 1], arr[i + 2])
+        if arr[i] > maxH:
+            ans = ans + (arr[i] - maxH)
+    print(f"#{test_case} {ans}")
 
