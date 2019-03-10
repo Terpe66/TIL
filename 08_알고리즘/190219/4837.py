d6f23b91f72253ba1307d168f974f9f2331bd8a8
Terpe66 <terpe66@gmail.com>
2019-03-10 23:22:44 +0900

    home | algorithm | 190310

--- 08_알고리즘/190219/4837.py
+++ 08_알고리즘/190219/4837.py
@@ -1,25 +1,37 @@
 import sys
 sys.stdin = open("4837.txt", "r")
 
+def a(n):
+    if n == 0:
+        return 0
+    return a(n-1) + n
+
+def bubun(n, k, sum_num=0):
+    global K, ans
+    sum_num += n
+    k -= 1
+    n += 1
+
+    if sum_num == K and k == 0:
+        ans += 1
+        return
+
+    if sum_num > K:
+        return
+
+    if k > 0:
+        while n < 13:
+            bubun(n, k, sum_num)
+            n += 1
+
 for t in range(int(input())):
-    n_k = list(map(int, input().split()))
+    N, K = map(int, input().split())
     A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     sum_list = []
-    for i in range(1 << len(A)):
-        SUM = 0
-        cnt = 0
-        for j in range(len(A)):
-            if i & (1 << j) != 0 and cnt < n_k[0]:
-                SUM += A[j]
-                cnt += 1
-        in_list = []
-        if SUM == n_k[1] and cnt == n_k[0]:
-            cnt = 0
-            for j in range(len(A)):
-                if i & (1 << j) != 0 and cnt < n_k[0]:
-                    in_list.append(A[j])
-                    cnt += 1
-            if in_list not in sum_list:
-                sum_list.append(in_list)
 
-    print("#{} {}".format(t+1, len(sum_list)))
+    ans = 0
+    for i in A:
+        bubun(i, N)
+
+
+    print(f"#{t+1} {ans}")
