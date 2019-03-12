fc4f008c83391c8f541abad79633e61cdd7bf272
Terpe66 <45963139+Terpe66@users.noreply.github.com>
2019-03-12 09:14:02 +0900

    algorithm | 190312

--- 08_알고리즘/190311/6485.py
+++ 08_알고리즘/190311/6485.py
@@ -22,3 +22,17 @@ for t in range(int(input())):
 
         print(cnt, end=" ")
     print()
+
+# T = int(input())
+# for test_case in range(1, T + 1):
+#     N = int(input())
+#     cnt = [0] * 5001
+#     for _ in range(N):
+#         A, B = map(int, input().split())
+#         for i in range(A, B + 1):
+#             cnt[i] += 1
+#     P = int(input())
+#     result = []
+#     for _ in range(P):
+#         C = int(input())
+#         result.append(cnt[C])
