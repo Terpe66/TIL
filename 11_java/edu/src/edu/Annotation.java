3f1aab3bfc2f165b5a504ccaabdfb36d0cf0c7a2
terpe66 <terpe66@gmail.com>
2020-08-06 18:23:02 +0900

    annotation info

--- 11_java/edu/src/edu/Annotation.java
+++ 11_java/edu/src/edu/Annotation.java
@@ -12,6 +12,16 @@ public class Annotation {
 	 * @return a + b 
 	 */
 	
+	/* 
+	 * @author [소스코드 작성자]
+	 * @param [매개변수 설명]
+	 * @return [반환값 설명]
+	 * @see [참조나 관련 있는 클래스]
+	 * @since [코드가 적용되기 시작한 버전 정보]
+	 * @throws [오류 설명]
+	 * @version [소스코드 버전]
+	 */
+	
 	public int sum(int a, int b) {
 		return a + b;
 	}
