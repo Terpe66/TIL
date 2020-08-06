88a6931a77f2e0cf70778e90ebaf840384e0276c
terpe66 <terpe66@gmail.com>
2020-08-06 18:21:16 +0900

    annotation

---
+++ 11_java/edu/src/edu/Annotation.java
@@ -0,0 +1,19 @@
+package edu;
+
+/*
+ * @author T
+ * 
+*/
+public class Annotation {
+	
+	/*
+	 * @param a 합을 구하려는 첫 번째 수
+	 * @param b 합을 구하려는 두 번째 수
+	 * @return a + b 
+	 */
+	
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+}
