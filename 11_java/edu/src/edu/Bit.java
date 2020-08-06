d4d35eb90a19dd7de0744e3c6bf01c6c88d7af39
terpe66 <terpe66@gmail.com>
2020-08-06 18:03:44 +0900

    shift

--- 11_java/edu/src/edu/Bit.java
+++ 11_java/edu/src/edu/Bit.java
@@ -10,6 +10,17 @@ public class Bit {
 		System.out.println(Integer.toBinaryString(c));
 		System.out.println(Integer.toBinaryString(d));
 		System.out.println(Integer.toBinaryString(e));
+		
+		int f = 12;
+		int g = (~f) + 1;
+		System.out.println(g);
+		
+		int h = f << 1;
+		int i = f >> 1;
+		System.out.println(h);
+		System.out.println(Integer.toBinaryString(f));
+		System.out.println(Integer.toBinaryString(h));
+		System.out.println(i);
 	}
 
 }
