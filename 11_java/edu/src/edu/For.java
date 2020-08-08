cc110641c903864a6c352fe35c6ff61295e16ef0
terpe66 <terpe66@gmail.com>
2020-08-08 16:28:59 +0900

    전위 후위 확인

---
+++ 11_java/edu/src/edu/For.java
@@ -0,0 +1,23 @@
+package edu;
+
+public class For {
+
+	public static void main(String[] args) {
+		int[][] testArr = new int[5][2];
+		
+		int i = 0;
+		for (int j = 0; j < testArr.length; ++j)
+		{
+			testArr[++i][0] = i;
+			testArr[j][1] = j;
+		}
+		
+		for (int j = 0; j < testArr.length; ++j)
+		{
+			System.out.print(testArr[j][0]);
+			System.out.print(", ");
+			System.out.println(testArr[j][1]);
+		}
+	}
+
+}
