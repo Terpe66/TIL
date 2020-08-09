f7c1573753eb48cc2162fdadee430dc39f80771e
terpe66 <terpe66@gmail.com>
2020-08-10 00:02:04 +0900

    rename OOP to Armor

---
+++ 11_java/edu/src/edu/Armor.java
@@ -0,0 +1,16 @@
+package edu;
+
+public class Armor {
+
+	private String name;
+	private int height;
+	private int weight;
+	private String color;
+	private boolean isFly;
+	
+	public void setName()
+	{
+		name = "mark6";
+	}
+
+}
