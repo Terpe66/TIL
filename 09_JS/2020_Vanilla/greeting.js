6458ca8075c08a34c839656e17fbee4507840ff4
terpe66 <terpe66@gmail.com>
2020-08-14 03:05:17 +0900

    add localStorage.setItem

--- 09_JS/2020_Vanilla/greeting.js
+++ 09_JS/2020_Vanilla/greeting.js
@@ -2,8 +2,21 @@ const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
 
-const userName = "Current User";
 const classShowing = "showing";
+
+function checkSubmit(event)
+{
+  event.preventDefault();
+  const inputName = input.value;
+  localStorage.setItem("username", inputName);
+  paintGreeting(inputName);
+}
+
+function getName()
+{
+  form.classList.add(classShowing);
+  form.addEventListener("submit", checkSubmit)
+}
 
 function paintGreeting(text)
 {
@@ -17,7 +30,7 @@ function loadName()
   const currentUser = localStorage.getItem("username");
   if (currentUser === null)
   {
-    
+    getName();
   } else {
     paintGreeting(currentUser);
   }
