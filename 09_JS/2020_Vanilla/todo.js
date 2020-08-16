1b75ac6a27c363ec0cb9fb104ab6296c6c745734
terpe66 <terpe66@gmail.com>
2020-08-17 03:04:00 +0900

    add delete todo

--- 09_JS/2020_Vanilla/todo.js
+++ 09_JS/2020_Vanilla/todo.js
@@ -2,15 +2,37 @@ const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event)
+{
+  const button = event.target;
+  const li = button.parentNode;
+  toDoList.removeChild(li);
+  toDos = toDos.filter(function (toDo)
+  {
+    return toDo.id !== parseInt(li.id);
+  })
+  toStorage();
+}
+
+function toStorage()
+{
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
 
 function saveToDo(text)
 {
   const toDo = document.createElement("li");
   const delButton = document.createElement("button");
   delButton.innerHTML = "❌";
+  delButton.addEventListener("click", deleteToDo);
   const span = document.createElement("span");
-  const idx = toDos.length + 1;
+  let idx = 1;
+  if (toDos.length !== 0)
+  {
+    idx = toDos[toDos.length - 1].id + 1;
+  }
   span.innerText = text;
   toDo.appendChild(span);
   toDo.appendChild(delButton);
@@ -21,7 +43,7 @@ function saveToDo(text)
     id: idx
   }
   toDos.push(toDoObj);
-  localStorage.setItem("toDos", JSON.stringify(toDos));
+  toStorage();
 }
 
 function toDoSubmit(event)
