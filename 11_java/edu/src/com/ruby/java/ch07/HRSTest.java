71f1a942bdbed19619ad92c2061260b5fff5cdf4
terpe66 <terpe66@gmail.com>
2020-08-28 01:46:04 +0900

    Update HRSTest.java

--- 11_java/edu/src/com/ruby/java/ch07/HRSTest.java
+++ 11_java/edu/src/com/ruby/java/ch07/HRSTest.java
@@ -1,25 +1,25 @@
 package com.ruby.java.ch07;
 
-abstract class Employee {
+abstract class Employee1 {
 	String name;
 	int salary;
 	
 	public abstract void calcSalary();
 }
 
-class Salesman extends Employee {
+class Salesman extends Employee1 {
 	public void calcSalary() {
 		System.out.println("Salesman : 기본 + 수당");
 	}
 }
 
-class Consultant extends Employee {
+class Consultant extends Employee1 {
 	public void calcSalary() {
 		System.out.println("Consultant : 기본 + 특별");
 	}
 }
 
-class Manager extends Employee {
+class Manager extends Employee1 {
 	public void calcSalary () {
 		System.out.println("Manager : 기본 + 팀");
 	}
