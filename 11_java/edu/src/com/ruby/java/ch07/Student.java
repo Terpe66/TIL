a30ae7bd09e52641ff0dc3fe25bb1d752075bc98
terpe66 <terpe66@gmail.com>
2020-08-27 13:31:51 +0900

    modify Student

--- 11_java/edu/src/com/ruby/java/ch07/Student.java
+++ 11_java/edu/src/com/ruby/java/ch07/Student.java
@@ -1,25 +1,7 @@
 package com.ruby.java.ch07;
 
-public class Student {
-	private String name;
-	private int age;
+public class Student extends Person {
 	private String major;
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
 	
 	public String getMajor() {
 		return major;
