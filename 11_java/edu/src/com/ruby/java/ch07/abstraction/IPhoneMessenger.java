ba67dc7cda993f3572cba137fb5f4f0a7fd09ec6
terpe66 <terpe66@gmail.com>
2020-08-28 21:57:25 +0900

    Create IPhoneMessenger.java

---
+++ 11_java/edu/src/com/ruby/java/ch07/abstraction/IPhoneMessenger.java
@@ -0,0 +1,15 @@
+package com.ruby.java.ch07.abstraction;
+
+public class IPhoneMessenger implements Messenger {
+	public String getMessage() {
+		return "iPhone";
+	}
+	
+	public void setMessage(String msg) {
+		System.out.println("iPhone msg : " + msg);
+	}
+	
+	public void clearMessage() {
+		System.out.println("clear!");
+	}
+}
