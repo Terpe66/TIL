5a09a8ffa3c0308eb8552810db2cca228ecd6c99
terpe66 <terpe66@gmail.com>
2020-10-09 20:38:41 +0900

    .

--- 11_java/edu/src/com/ruby/java/test1/Exam01.java
+++ 11_java/edu/src/com/ruby/java/test1/Exam01.java
@@ -1,5 +1,11 @@
 package com.ruby.java.test1;
 
 public class Exam01 {
-
+	public static void main(String[] args) {
+		String test = "1.2";
+		System.out.println(test.getClass());
+		
+		double db = Double.parseDouble(test);
+		System.out.println());
+	}
 }
